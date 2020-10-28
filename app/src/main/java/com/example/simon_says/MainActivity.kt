fe052a07621936af8636832e20c65592d431d74e
dcancelas <dcancelasgarcia@danielcastelao.org>
2020-10-28 14:25:15 +0100

    Some changes

--- app/src/main/java/com/example/simon_says/MainActivity.kt
+++ app/src/main/java/com/example/simon_says/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.simon_says
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val greenButton: Button = findViewById(R.id.green_button)
+        greenButton.setOnClickListener {
+            greenButton.background.setTint(getColor(R.color.green_pressed))
+        }
+        val redButton: Button = findViewById(R.id.red_button)
+        redButton.setOnClickListener {
+            Toast.makeText(this, "red is selected", Toast.LENGTH_SHORT).show()
+        }
+        val yellowButton: Button = findViewById(R.id.yellow_button)
+        yellowButton.setOnClickListener {
+            Toast.makeText(this, "yellow is selected", Toast.LENGTH_SHORT).show()
+        }
+        val blueButton: Button = findViewById(R.id.blue_button)
+        blueButton.setOnClickListener {
+            Toast.makeText(this, "blue is selected", Toast.LENGTH_SHORT).show()
+        }
     }
 }
