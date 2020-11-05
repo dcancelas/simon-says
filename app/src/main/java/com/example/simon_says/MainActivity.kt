cd5de7049d1162d34773de44e3ff8c7ddabe32c7
dcancelas <dcancelasgarcia@danielcastelao.org>
2020-11-05 12:37:39 +0100

    Probando cosas

--- app/src/main/java/com/example/simon_says/MainActivity.kt
+++ app/src/main/java/com/example/simon_says/MainActivity.kt
@@ -1,33 +1,76 @@
 package com.example.simon_says
 
-import android.content.res.ColorStateList
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttons: Array<Button>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val playButton: Button = findViewById(R.id.play_button)
         val greenButton: Button = findViewById(R.id.green_button)
-        greenButton.setOnClickListener {
-            Toast.makeText(this, "green is selected", Toast.LENGTH_SHORT).show()
-        }
         val redButton: Button = findViewById(R.id.red_button)
-        redButton.setOnClickListener {
-            Toast.makeText(this, "red is selected", Toast.LENGTH_SHORT).show()
-        }
         val yellowButton: Button = findViewById(R.id.yellow_button)
-        yellowButton.setOnClickListener {
-            Toast.makeText(this, "yellow is selected", Toast.LENGTH_SHORT).show()
-        }
         val blueButton: Button = findViewById(R.id.blue_button)
+        buttons = arrayOf(playButton,greenButton,redButton,yellowButton,blueButton)
+
+        playButton.setOnClickListener {
+            for (b in buttons) {
+                b.isEnabled = false
+            }
+            GlobalScope.launch(Dispatchers.IO) {
+                startGame()
+                /*withContext(Dispatchers.Main) {
+                    for (b in buttons) {
+                        b.isEnabled = true
+                    }
+                }*/
+            }
+        }
+        greenButton.setOnClickListener {
+
+        }
+        redButton.setOnClickListener {
+
+        }
+        yellowButton.setOnClickListener {
+
+        }
         blueButton.setOnClickListener {
-            Toast.makeText(this, "blue is selected", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    private suspend fun startGame() {
+        for (x in 1..4) {
+            when (x) {
+                1 -> {
+                    buttons[1].setBackgroundResource(R.drawable.green_pressed_button)
+                    delay(1000)
+                    buttons[1].setBackgroundResource(R.drawable.green_button)
+                }
+                2 -> {
+                    buttons[2].setBackgroundResource(R.drawable.red_pressed_button)
+                    delay(1000)
+                    buttons[2].setBackgroundResource(R.drawable.red_button)
+                }
+                3 -> {
+                    buttons[3].setBackgroundResource(R.drawable.yellow_pressed_button)
+                    delay(1000)
+                    buttons[3].setBackgroundResource(R.drawable.yellow_button)
+                }
+                4 -> {
+                    buttons[4].setBackgroundResource(R.drawable.blue_pressed_button)
+                    delay(1000)
+                    buttons[4].setBackgroundResource(R.drawable.blue_button)
+                }
+            }
         }
     }
 }
