f8ef733e77cf6ac34e043fae368ac24eb1f7eaba
dcancelas <dcancelasgarcia@danielcastelao.org>
2020-11-09 10:51:39 +0100

    Interfaz terminada

--- app/src/main/java/com/example/simon_says/MainActivity.kt
+++ app/src/main/java/com/example/simon_says/MainActivity.kt
@@ -2,17 +2,25 @@ package com.example.simon_says
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var buttons: Array<Button>
+    private var running: Boolean = false
+    private var score: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val s: TextView = findViewById(R.id.score)
+        s.text = score.toString()
+        val bs: TextView = findViewById(R.id.best_score)
+        bs.text = score.toString()
         val playButton: Button = findViewById(R.id.play_button)
         val greenButton: Button = findViewById(R.id.green_button)
         val redButton: Button = findViewById(R.id.red_button)
@@ -21,16 +29,9 @@ class MainActivity : AppCompatActivity() {
         buttons = arrayOf(playButton,greenButton,redButton,yellowButton,blueButton)
 
         playButton.setOnClickListener {
-            for (b in buttons) {
-                b.isEnabled = false
-            }
-            GlobalScope.launch(Dispatchers.IO) {
-                startGame()
-                /*withContext(Dispatchers.Main) {
-                    for (b in buttons) {
-                        b.isEnabled = true
-                    }
-                }*/
+            if (!running) startGame()
+            else {
+
             }
         }
         greenButton.setOnClickListener {
@@ -47,29 +48,45 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private suspend fun startGame() {
-        for (x in 1..4) {
-            when (x) {
-                1 -> {
-                    buttons[1].setBackgroundResource(R.drawable.green_pressed_button)
-                    delay(1000)
-                    buttons[1].setBackgroundResource(R.drawable.green_button)
-                }
-                2 -> {
-                    buttons[2].setBackgroundResource(R.drawable.red_pressed_button)
-                    delay(1000)
-                    buttons[2].setBackgroundResource(R.drawable.red_button)
-                }
-                3 -> {
-                    buttons[3].setBackgroundResource(R.drawable.yellow_pressed_button)
-                    delay(1000)
-                    buttons[3].setBackgroundResource(R.drawable.yellow_button)
-                }
-                4 -> {
-                    buttons[4].setBackgroundResource(R.drawable.blue_pressed_button)
-                    delay(1000)
-                    buttons[4].setBackgroundResource(R.drawable.blue_button)
-                }
+    private fun startGame() {
+        GlobalScope.launch {
+            running = true
+            for (x in 1..4) {
+                setButton(x,"unpressed")
+            }
+            for (x in 1..4) {
+                setButton(x,"pressed")
+                delay(1000)
+                setButton(x,"unpressed")
+            }
+            for (x in 1..4) {
+                setButton(x,"default")
+            }
+            running = false
+        }
+    }
+
+    private fun setButton(button: Int, state: String) {
+        when (button) {
+            1 -> {
+                if (state == "default") buttons[1].setBackgroundResource(R.drawable.green_button)
+                if (state == "pressed") buttons[1].setBackgroundResource(R.drawable.green_pressed_button)
+                if (state == "unpressed") buttons[1].setBackgroundResource(R.drawable.green_unpressed_button)
+            }
+            2 -> {
+                if (state == "default") buttons[2].setBackgroundResource(R.drawable.red_button)
+                if (state == "pressed") buttons[2].setBackgroundResource(R.drawable.red_pressed_button)
+                if (state == "unpressed") buttons[2].setBackgroundResource(R.drawable.red_unpressed_button)
+            }
+            3 -> {
+                if (state == "default") buttons[3].setBackgroundResource(R.drawable.yellow_button)
+                if (state == "pressed") buttons[3].setBackgroundResource(R.drawable.yellow_pressed_button)
+                if (state == "unpressed") buttons[3].setBackgroundResource(R.drawable.yellow_unpressed_button)
+            }
+            4 -> {
+                if (state == "default") buttons[4].setBackgroundResource(R.drawable.blue_button)
+                if (state == "pressed") buttons[4].setBackgroundResource(R.drawable.blue_pressed_button)
+                if (state == "unpressed") buttons[4].setBackgroundResource(R.drawable.blue_unpressed_button)
             }
         }
     }
